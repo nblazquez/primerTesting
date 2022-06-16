74444fb6839874578226006811e7288d32c902b4
Nazareth Blazquez <blazquezjimenezn@iesmartinrivero.org>
2022-06-16 12:34:01 +0200

    Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { getMeaning } from './dictionary-service';
 
-function App() {
+const App = () => {
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [wordMeaning, setWordMeaning] = React.useState('');
+  const [errorMsg, setErrorMsg] = React.useState('');
+  
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      setIsLoading(true);
+      setWordMeaning('');
+      setErrorMsg('');
+      const {word} = e.target.elements;
+      const response = await getMeaning({word: word.value});
+      setWordMeaning(response.data[0].meanings[0].definitions[0].definition);
+    } catch (e) {
+      if (e.message.includes('404')) {
+        setErrorMsg('Word not found :( Try another search');
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Free Dictionary</h1>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='word'>Word: {` `}</label>
+        <input type='text' id='word'/>
+
+        <button disabled={isLoading} type='submit'>Consult</button>
+      </form>
+
+      {isLoading && <p>Loading...</p>}
+
+      {!isLoading && wordMeaning && <p>{wordMeaning}</p>}
+
+      {!isLoading && !wordMeaning && !errorMsg && <p>Type a word and click on Consult</p>}
+
+      {!isLoading && errorMsg && <p>{errorMsg}</p>}
+
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,71 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import axios from 'axios';
 import App from './App';
 
-test('renders learn react link', () => {
+jest.mock('axios');
+
+const getFakeResponse = ({expectedDefinition}) => ({
+  data: [{
+    meanings: [{
+      definitions: [{
+        definition: expectedDefinition
+      }]
+    }]
+  }]
+});
+
+beforeEach(() => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  axios.get.mockClear();
+});
+
+const fillFormAndSubmit = () => {
+  const inputEl = screen.getByLabelText(/word/i);
+  const btnEl = screen.getByRole('button', { name: /consult/i});
+
+  fireEvent.change(inputEl, {target: {value: 'house'}});
+  fireEvent.click(btnEl);
+}
+
+test('renders Free Dictionary title', () => {
+  const title = screen.getByText(/free dictionary/i);
+  expect(title).toBeInTheDocument();
+});
+
+test('should render the form elements', () => {
+  const inputEl = screen.getByLabelText(/word/i);
+  const btnEl = screen.getByRole('button', { name: /consult/i});
+
+  expect(inputEl).toBeInTheDocument();
+  expect(btnEl).toBeInTheDocument();
+});
+
+test('should search a word', async () => {
+  const expectedDefinition = 'A structure built or serving as an abode of human beings';
+  axios.get.mockReturnValueOnce(getFakeResponse({expectedDefinition}));
+  
+  fillFormAndSubmit();
+
+  const wordMeaningEl = await screen.findByText(/A structure built or serving as an abode of human beings/i);
+  expect(wordMeaningEl).toBeInTheDocument();
+
+  screen.debug();
+});
+
+test('should not exist Loading string when search is finished', async () => {
+  const expectedDefinition = 'A structure built or serving as an abode of human beings';
+  axios.get.mockReturnValueOnce(getFakeResponse({expectedDefinition}));
+
+  fillFormAndSubmit();
+
+  const loadingEl = screen.getByText(/loading/i);
+
+  expect(loadingEl).toBeInTheDocument();
+
+  const wordMeaningEl = await screen.findByText(expectedDefinition);
+  
+  const loadingElExpectedEl = screen.queryByText(/loading/i);
+  expect(loadingElExpectedEl).not.toBeInTheDocument();
+  
 });
